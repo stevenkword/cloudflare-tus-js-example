f8bd645432c7914bbc0dc8a6050be59b47f1425e
Steven K Word <stevenword@gmail.com>
2018-12-20 13:30:37 -0600

    Auth Key Support

---
+++ auth.js
@@ -0,0 +1,3 @@
+var xAuthEmail = '';
+var xAuthKey   = '';
+var zone       = '';
--- index.js
+++ index.js
@@ -1,34 +1,60 @@
-//import tus from "tus-js-client";
+jQuery( document ).ready(function($) {
 
-var input = jQuery('#cloudflare-stream-file-input');
+    var myInput    = jQuery('#cloudflare-stream-file-input');
 
-input.addEventListener("change", function(e) {
+    myInput.on("change", function(e) {
 
-    var file = jQuery("#cloudflare-stream-file-input:input[type='file']")[0].files[0];
-    console.log(file);
+        var file = jQuery("#cloudflare-stream-file-input:input[type='file']")[0].files[0];
 
-    var responseInput = jQuery('#cloudflare-stream-response');
+        var data = {
+            'filename': file.name
+        };
 
-    // Create a new tus upload
-    var upload = new tus.Upload(file, {
-        endpoint: response.data.location,
-        retryDelays: [0, 1000, 3000, 5000],
-        headers: {
-            'X-Auth-Email':'steven.word@wpengine.com',
-            'X-Auth-Key':'36c6251c7ed10da7a808918b05e480e89298e',
-        },
-        onError: function(error) {
-            responseInput.text("Failed because: " + error);
-        },
-        onProgress: function(bytesUploaded, bytesTotal) {
-            var percentage = (bytesUploaded / bytesTotal * 100).toFixed(2);
-            responseInput.text(bytesUploaded, bytesTotal, percentage + "%")
-        },
-        onSuccess: function() {
-            console.log("Download %s from %s", upload.file.name, upload.url)
-            responseInput.text( "Complete" );
-        }
+        var responseInput = jQuery('#cloudflare-stream-response');
+        var base_url = 'https://api.cloudflare.com/client/v4/zones/' + zone + '/media';
+
+        $.ajax({
+            url: base_url,
+            type: 'post',
+            contentType: 'application/x-www-form-urlencoded',
+            headers: {
+                'X-Auth-Email' : xAuthEmail,
+                'X-Auth-Key'   : xAuthKey,
+                'Tus-Resumable': '1.0.0',
+                'Upload-Length': file.size
+            },
+            data: data,
+            success: function( data, textStatus, response ){
+                responseInput.text("Uploading...");
+
+                // Create a new tus upload
+                var upload = new tus.Upload(file, {
+
+                    endpoint: response.getResponseHeader('location'),
+                    retryDelays: [0, 1000, 3000, 5000],
+                    headers: {
+                        'X-Auth-Email' : xAuthEmail,
+                        'X-Auth-Key'   : xAuthKey,
+                    },
+                    onError: function(error) {
+                        responseInput.text("Failed because: " + error);
+                    },
+                    onProgress: function(bytesUploaded, bytesTotal) {
+                        var percentage = (bytesUploaded / bytesTotal * 100).toFixed(2);
+                        responseInput.text(bytesUploaded, bytesTotal, percentage + "%")
+                    },
+                    onSuccess: function() {
+                        console.log("Download %s from %s", upload.file.name, upload.url)
+                        responseInput.text( "Complete" );
+                    }
+                })
+                // Start the upload
+                upload.start();
+
+            },
+            error: function( jqXhr, textStatus, errorThrown ){
+                console.log( errorThrown );
+            }
+        });
     })
-    // Start the upload
-    upload.start();
-})
+});
